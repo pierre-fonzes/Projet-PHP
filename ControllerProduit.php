77217027322edc06be624fc565595a343227cc2b
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2021-10-20 17:27:06 +0200

    commit du controllerProduit et ModelProduit
    ajout des fichiers ControllerProduit.php et ModelProduit.php

---
+++ ControllerProduit.php
@@ -0,0 +1,44 @@
+<?php
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
+class ControllerProduit {
+    public static function readAll() {
+        $tab_v = ModelProduit::getAllProduits();     //appel au modèle pour gerer la BD
+        $controller='produit';
+        $view='list';
+        $pagetitle='Liste des produits';
+        require ('../view/produit/view.php');  //"redirige" vers la vue
+
+    }
+    public static function read(){
+    	$numProduit = $_GET['numProduit'];
+    	$v = ModelProduit::getProduitBynum($numProduit);//appel au modèle pour gerer la BD
+        $controller='produit';
+    	if($v == false){
+            $view = 'error';
+            $pagetitle = 'Produit inconnu';
+    		require ('../view/produit/view.php');//redirige vers la vue de produit non reconnu 
+    	}else{
+            $view = 'detail';
+            $pagetitle = 'Détail du Produit';    
+        	require ('../view/produit/view.php');  //"redirige" vers la vue
+    	}
+    } 
+    public static function create(){
+        $view = 'create';
+        $pagetitle  = 'Creation d''un produit';
+        $controller = 'produit';
+    	require('../view/produit/create.php'); //redirige vers la vue de création
+    }
+    //à faire pour la prochaine fois
+    public static function created(){
+    	$immat = $_GET['immatriculation'];
+    	$marque = $_GET['marque'];
+    	$couleur = $_GET['couleur'];
+    	$voiture = new ModelVoiture($immat,$marque,$couleur);
+    	$voiture->save();
+    	ControllerVoiture::readAll();
+        require_once '../view/voiture/created.php';//il faudrait en théorie utiliser la méthode File::build_path() mais j'ai la flemme 
+        require_once '../view/voiture/list.php';
+    }
+}
+?>
---
+++ ModelProduit.php
@@ -0,0 +1,107 @@
+<?php
+
+class ModelProduit {
+
+
+    private $numProduit;
+    private $nomProduit;
+    private $idCategorie;
+    private $prix;
+    private $description;
+
+    // un getter      
+    public function getnumProduit() {
+        return $this->numProduit;
+    }
+
+    // un setter 
+    public function setPrix($m) {
+        $this->prix = $m;
+    }
+    public function getnomProduit(){
+        return $this->nomProduit;
+    }
+    public function setdescription($c){
+        if(strlen($c)<=30){
+            $this->description = $c;
+        }
+    }
+    public function getPrix(){
+        return $this->prix;
+    }
+    public static function getProduitBynum($nume) {
+        require_once "Model.php";
+        $sql = "SELECT * from p_Produit WHERE numProduit=:nom_tag";
+        // Préparation de la requête
+        try{
+            $req_prep = Model::getPDO()->prepare($sql);
+
+            $values = array(
+                "nom_tag" => $nume,
+                //nomdutag => valeur, ...
+            );
+            // On donne les valeurs et on exécute la requête     
+            $req_prep->execute($values);
+
+            // On récupère les résultats comme précédemment
+            $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelProduit');
+            $tab_prod = $req_prep->fetchAll();
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        // Attention, si il n'y a pas de résultats, on renvoie false
+        if (empty($tab_voit))
+            return false;
+        return $tab_voit[0];
+    }
+    public  function save(){
+        require_once 'Model.php';
+        //'". str_replace( "'", "''", $s ) ."' 
+        $sql = "INSERT INTO p_Produit(nomProduit, idCategorie, numProduit, prix, description)VALUES('$this->nomProduit','$this->idCategorie','$this->numProduit' ,'$this->prix','$this->description')";
+        //echo $sql;
+        //die();
+        // Préparation de la requête
+        try{
+            $req_prep = Model::getPDO()->prepare($sql);
+
+            $values = array(
+                "nomProduit" => $this->nomProduit,
+                "idCategorie" => $this->idCategorie,
+                "numProduit" => $this->numProduit,
+                "prix" => $this->prix;
+                "description" => $this->description;
+            //nomdutag => valeur, ...
+            );
+
+            $req_prep->execute($values);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+
+    public static function getAllProduits(){
+        require_once "Model.php";
+        $rep =  Model::getPDO()->query("SELECT * FROM p_Produit");
+        $rep->setFetchMode(PDO::FETCH_CLASS, 'ModelProduit');
+        $tab_prod = $rep->fetchAll();
+        return $tab_voit;    }
+
+    // un constructeur
+    public function __construct($nume = NULL, $prix = NULL, $id = NULL , $nom=NULL,$desc=NULL) {
+        if(!is_null($nume)&& !is_null($prix)&& !is_null($id) && !is_null($nom) && !is_null($desc)){
+            $this->numProduit = $nume;
+            $this->prix = $prix;
+            $this->idCategorie= $id;
+            $this->nomProduit = $nom;
+            $this->description = $desc;
+        }
+    } 
+
+    // une methode d'affichage.
+    /*public function afficher() {
+      echo $this->marque;
+  }*/
+}
+?>
+
